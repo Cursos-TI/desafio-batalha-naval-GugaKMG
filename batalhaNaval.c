2e1a3601ad2693a864fb4a31715257dfda449b65
GugaKMG <gugakmg1@gmail.com>
2025-07-23 09:28:14 -0300

    Nível intermediário

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main(){
     int navio1[3] = {3, 3, 3},
-        navio2[3] = {3, 3, 3};
+        navio2[3] = {3, 3, 3},
+        navio3[3] = {3, 3, 3},
+        navio4[3] = {3, 3, 3};
 
     int tabuleiro[10][10] =
     {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -36,13 +38,31 @@ int main() {
         exit(1);
      }
 
+     if(tabuleiro[0][0] == 0 && tabuleiro[1][1] == 0 && tabuleiro[2][2] == 0){
+        tabuleiro[0][0] = navio3[0];
+        tabuleiro[1][1] = navio3[1];
+        tabuleiro[2][2] = navio3[2];
+     } else{
+        printf("Navios sobrepostos.");
+        exit(1);
+     }
+
+     if(tabuleiro[7][7] == 0 && tabuleiro[7][8] == 0 && tabuleiro[7][9] == 0){
+        tabuleiro[7][7] = navio4[0];
+        tabuleiro[8][8] = navio4[1];
+        tabuleiro[9][9] = navio4[2];
+     } else{
+        printf("Navios sobrepostos.");
+        exit(1);
+     }
+
     //Exibição do tabuleiro.
     for(int i = 0; i <= 9; i++){
         for(int j = 0; j <= 9; j++){
-                printf(" |%d| ", tabuleiro[i][j]);
+                printf(" %d ", tabuleiro[i][j]);
             }
 
-        printf("\n-------------------------------------------------\n");
+        printf("\n");
     }
 
     return 0;
